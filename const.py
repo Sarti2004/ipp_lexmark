029104293940bca4fc2e3baa0d08088d2da98bc6
Sarti2004 <65831791+Sarti2004@users.noreply.github.com>
2023-05-26 14:43:44 -0700

    Initial commit

---
+++ const.py
@@ -0,0 +1,20 @@
+"""Constants for the IPP integration."""
+
+# Integration domain
+DOMAIN = "ipp_lexmark"
+
+# Attributes
+ATTR_COMMAND_SET = "command_set"
+ATTR_INFO = "info"
+ATTR_MARKER_TYPE = "marker_type"
+ATTR_MARKER_LOW_LEVEL = "marker_low_levels"
+ATTR_MARKER_HIGH_LEVEL = "marker-high-levels"
+ATTR_SERIAL = "serial"
+ATTR_STATE_MESSAGE = "state_message"
+ATTR_STATE_REASON = "printer_state_reasons"
+ATTR_URI_SUPPORTED = "uri_supported"
+
+# Config Keys
+CONF_BASE_PATH = "base_path"
+CONF_SERIAL = "serial"
+CONF_TLS = "tls"
