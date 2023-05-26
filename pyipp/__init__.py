2d7310177f82369cc88f7a203648532167d44a90
Sarti2004 <65831791+Sarti2004@users.noreply.github.com>
2023-05-26 14:47:44 -0700

    ipp library added

---
+++ pyipp/__init__.py
@@ -0,0 +1,13 @@
+"""Asynchronous Python client for IPP."""
+from .ipp import (  # noqa
+    IPP,
+    IPPConnectionError,
+    IPPConnectionUpgradeRequired,
+    IPPError,
+    IPPParseError,
+    IPPResponseError,
+    IPPVersionNotSupportedError,
+)
+from .models import Printer  # noqa
+
+__version__ = "1.02"
